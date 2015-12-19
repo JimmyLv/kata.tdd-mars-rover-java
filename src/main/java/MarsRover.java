07c8710f6d5d45ac3928c26adaa66fdcf4a5217c
Yan Qian <qianyan@thoughtworks.com>
2015-12-19 11:40:00 +0800

    [Yan Qian & Congyu Lin] MarsRover should execute a right turn command.

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,13 +1,20 @@
 public class MarsRover {
+
+    private String orientation;
+
     public MarsRover(String initialPositionAndOrientation) {
 
     }
 
     public void execute(String command) {
-
+        if (command.equals("R")) {
+            orientation = "E";
+        } else if (command.equals("L")) {
+            orientation = "W";
+        }
     }
 
     public String getPositionAndOrientation() {
-        return "0 0 W";
+        return "0 0 " + orientation;
     }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,15 +1,32 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class MarsRoverTest {
+
+    private MarsRover marsRover;
+
+    @Before
+    public void setUp() throws Exception {
+        marsRover = new MarsRover("0 0 N");
+    }
+
     @Test
     public void should_return_00W_when_received_a_left_turn_command() throws Exception {
-        MarsRover marsRover = new MarsRover("0 0 N");
+        MarsRover marsRover = this.marsRover;
 
         marsRover.execute("L");
 
         assertThat(marsRover.getPositionAndOrientation(), is("0 0 W"));
+    }
+
+    @Test
+    public void should_return_00E_when_received_a_right_turn_command() throws Exception {
+
+        marsRover.execute("R");
+
+        assertThat(marsRover.getPositionAndOrientation(), is("0 0 E"));
     }
 }
