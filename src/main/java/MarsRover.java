e875dd4db3b9cb942562bef44fdf53d15612ade7
Jimmy Lv <jinglv@thoughtworks.com>
2015-12-25 21:18:26 +0800

    [Kimmy & Jimmy] refactoring using Position

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -11,11 +11,13 @@ public class MarsRover {
     private int y;
     private int x;
     private String orientation;
+    private Position position;
 
     public MarsRover(String initialPosition, String initialOrientation) {
         String[] strings = initialPosition.split(" ");
         this.x = Integer.parseInt(strings[0]);
         this.y = Integer.parseInt(strings[1]);
+        this.position = new Position(x, y);
         this.orientation = initialOrientation;
     }
 
@@ -39,6 +41,13 @@ public class MarsRover {
         return command == 'R';
     }
 
+    private void rotateLeft() {
+        int index = ORIENTATIONS.indexOf(orientation);
+        int pre = index - 1;
+        pre = pre < 0 ? ORIENTATIONS.size() + pre : pre;
+        orientation = ORIENTATIONS.get(pre);
+    }
+
     private void rotateRight() {
         int index = ORIENTATIONS.indexOf(orientation);
         int next = index + 1;
@@ -49,21 +58,20 @@ public class MarsRover {
     private void moveForward() {
         if (orientation.equals("N")) {
             this.y += 1;
+            position.move(0, 1);
         } else if (orientation.equals("W")) {
             this.x -= 1;
+            position.move(-1, 0);
         } else if (orientation.equals("E")) {
             this.x += 1;
+            position.move(1, 0);
+        } else if (orientation.equals("S")) {
+            this.y -= 1;
+            position.move(0, -1);
         }
     }
 
-    private void rotateLeft() {
-        int index = ORIENTATIONS.indexOf(orientation);
-        int pre = index - 1;
-        pre = pre < 0 ? ORIENTATIONS.size() + pre : pre;
-        orientation = ORIENTATIONS.get(pre);
-    }
-
     public String getPositionAndOrientation() {
-        return this.x + " " + this.y + " " + orientation;
+        return String.format("%s %s", this.position, orientation);
     }
 }
---
+++ src/main/java/Position.java
@@ -0,0 +1,19 @@
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move(int offsetX, int offsetY) {
+        this.x += offsetX;
+        this.y += offsetY;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %d", x, y);
+    }
+}
